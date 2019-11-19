6033e7feeb52dbe822e3edce57ee8ba03f455cac
Pierre Verbe <pierre.verbe@free.fr>
2019-11-19 22:35:24 +0100

    add the 3D cube

--- Arduino/Leanometer/Leanometer.ino
+++ Arduino/Leanometer/Leanometer.ino
@@ -28,8 +28,8 @@ void setup()
 
   Wire.begin();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.display();
-  delay(500); // Pause for 0.5 seconde
+  /*display.display();
+  delay(500); // Pause for 0.5 seconde*/
   display.clearDisplay();// Clear the buffer
 
   display.setTextSize(2);             // Normal 1:1 pixel scale
@@ -42,48 +42,38 @@ void setup()
   display.display();      // Show initial text
   delay(500);
 
-  //Wire.endTransmission(OLED);
+  /*Wire.endTransmission(OLED);*/
 
-  // initialisation des fonctions
-  //testdrawstyles();
-
-  //Wire.beginTransmission(MPU);
-  //Wire.write(0x6B);  // PWR_MGMT_1 register
-  //Wire.write(0);     // set to zero (wakes up the MPU-6050)
-  //Wire.endTransmission(true);
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
 }
 
 void loop()
 {
-  /*Wire.beginTransmission(MPU);
-    Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU, 14, true); // request a total of 14 registers
-    AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-    AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-    AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-    GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-    GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-    GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-    Serial.print("AcX = ");
-    Serial.println(AcX / 180);
-    Serial.print("AcY = ");
-    Serial.println(AcY / 180);
-    Serial.print("AcZ = ");
-    Serial.println(AcZ / 180);
-
-    /*Serial.print("GyX = ");
-    Serial.println(GyX);
-    Serial.print("GyY = ");
-    Serial.println(GyY);
-    Serial.print("GyZ = ");
-    Serial.println(GyZ);*/
-  /*Serial.println();
-    Serial.println();*/
-
-
-
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(true);
+  Wire.requestFrom(MPU, 14, true); // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+   Serial.println("Test terminal");
+  Serial.print("AcX = ");
+  Serial.println(AcX / 180);
+  Serial.print("AcY = ");
+  Serial.println(AcY / 180);
+  Serial.print("AcZ = ");
+  Serial.println(AcZ / 180);
+  Serial.println();
+  Serial.println();
+  
+  /*printAngleOLED(AcX / 180);*/
   /*compteur++;
     if (compteur == 255)compteur = 0;
 
@@ -101,23 +91,23 @@ void loop()
     {
     display.println(F(";)"));
     }*/
-  Serial.println(compteur);
+  /*Serial.println(compteur);*/
 
   /*while (compteur > 0) {
     printAngleOLED(compteur);
     display.display();
     delay(200);
     compteur--;
-  }
-  while (compteur < 90) {
+    }
+    while (compteur < 90) {
     printAngleOLED(compteur);
     display.display();
     delay(200);
     compteur++;
-  }*/
+    }*/
 
 
-  printInfo();
+  /*printInfo();*/
 
   /*lcd.begin(16, 2);
     lcd.setCursor(1,1);
@@ -204,12 +194,12 @@ void printGyro(int16_t AcX, int16_t AcY, int16_t AcZ) {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.println("GYRO MPU6050");
-  display.print("AcX = ");
+  display.println(F("GYRO MPU6050"));
+  display.print(F("AcX = "));
   display.println(AcX);
-  display.print("AcY = ");
+  display.print(F("AcY = "));
   display.println(AcY);
-  display.print("AcZ = ");
+  display.print(F("AcZ = "));
   display.println(AcZ);
 }
 
